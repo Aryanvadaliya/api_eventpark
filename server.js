f5977e859ff13ba084a9e77fef3ce4d84fb7f338
Aryan <128956837+Aryanvadaliya@users.noreply.github.com>
2025-03-03 19:08:45 +0530

    create intent fix

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const stripe = Stripe("sk_test_tR3PYbcVNZZ796tH88S4VQ2u");
 app.use(cors());
 app.use(express.json());
 
-// Define your custom route BEFORE json-server router
 app.post("/get-intent", async (req, res) => {
   try {
     const { amount } = req.body;
@@ -20,7 +19,8 @@ app.post("/get-intent", async (req, res) => {
 
     const paymentIntent = await stripe.paymentIntents.create({
       amount,
-      currency: "inr",
+      currency: "usd",
+      description: "learning stripe"
     });
 
     res.json({ clientSecret: paymentIntent.client_secret });
@@ -30,12 +30,10 @@ app.post("/get-intent", async (req, res) => {
   }
 });
 
-// json-server middleware and router
 const middlewares = jsonServer.defaults();
 app.use(middlewares);
 app.use(jsonServer.router("db.json"));
 
-// Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
